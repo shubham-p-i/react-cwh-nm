7634c2a4270c273f8de03faee16e329e5fa29b07
Shubham Patil <shubham.p@ibirdsservices.com>
2023-04-28 13:26:59 +0530

    using fetch api

--- src/components/News.js
+++ src/components/News.js
@@ -2,21 +2,39 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+
+  constructor(){
+    super();
+    console.log("Im super in news comp");
+
+    this.state = {
+      articles : []
+    }
+
+  }
+
+  async componentDidMount(){
+
+    let response = await fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=23a9ff7dca9a4c879fc6ff9b22196288");
+
+    let parsedData = await response.json();
+  
+    this.setState({articles:parsedData.articles});
+  }
+
   render() {
     return (
       <div className="container my-3">
-        <h2 className="">Top Headlines</h2>
+        <h1 className="">LATEST NEWS</h1>
+
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="my title" desc="my desc" />
+          {this.state.articles.map((ele)=>{
+           return <div className="col-md-3" key={ele.url}>
+            <NewsItem title={ele.title} desc={ele.description} imgUrl={ele.urlToImage} newsUrl={ele.url}/>
           </div>
-          <div className="col-md-4">
-            <NewsItem title="my title" desc="my desc" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="my title" desc="my desc" />
-          </div>
+          })}
         </div>
+
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,16 +3,22 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
   render() {
 
-    let {title,desc} = this.props;
+    let {title,desc,imgUrl,newsUrl} = this.props;
 
     return (
       <div>
-        <div className="card" style={{width: "18rem"}}>
-          <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" className="card-img-top" alt="..." />
+        <div className="card my-2 mx-2 text-center" style={{ width: "18rem" }}>
+          <img src={imgUrl ? imgUrl : "https://i.pinimg.com/originals/4e/a9/81/4ea98108e0dd10a1dc417a24e32232e2.png"} className="card-img-top" alt="..." style={{
+            width: "286px",
+            height: "175px",
+            objectFit: "cover"
+          }} />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{desc}</p>
-            <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+            {/* <h5 className="card-title">{title?title.slice(0,50):""}...</h5> */}
+            <h5 className="card-title">{title ? title : ""}...</h5>
+            {/* <p className="card-text">{desc?desc.slice(0,100):""}...</p> */}
+            <p className="card-text">{desc ? desc : ""}...</p>
+            <a href={newsUrl} target="_blank" className="btn btn-primary">Read more</a>
           </div>
         </div>
       </div>
