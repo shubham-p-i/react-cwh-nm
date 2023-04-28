5adb7996512aeb5a4629b58998d543fbd024250a
shubham-p-i <shubham.p@ibirdsservices.com>
2023-04-29 01:17:47 +0530

    added next previous buttons

--- src/components/News.js
+++ src/components/News.js
@@ -2,55 +2,60 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-
-  constructor(){
+  constructor() {
     super();
     console.log("Im super in news comp.");
 
     this.state = {
-      articles : [],
-      page : 1
-    }
-
+      articles: [],
+      page: 1,
+    };
   }
 
-  async componentDidMount(){
-
-    let response = await fetch("https://newsapi.org/v2/top-headlines?country=in&apiKey=23a9ff7dca9a4c879fc6ff9b22196288&page=1&pageSize=20");
+  async componentDidMount() {
+    let response = await fetch(
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=23a9ff7dca9a4c879fc6ff9b22196288&page=1&pageSize=20"
+    );
 
     let parsedData = await response.json();
-  
-    this.setState({articles:parsedData.articles});
+
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
 
-  handleNavigation = async (e) =>{
-    console.log("e.target.name --> ",e.target.name);
-    if(e.target.name === "next"){
+    handleNavigation = async (e) => {
+      if (e.target.name === "next") {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+        } else {
+          let response = await fetch(
+            `https://newsapi.org/v2/top-headlines?country=in&apiKey=23a9ff7dca9a4c879fc6ff9b22196288&page=${
+              this.state.page + 1
+            }&pageSize=20`
+          );
 
-      this.setState({
-        page : this.state.page + 1
-      })
+          let parsedData = await response.json();
 
-      let response = await fetch( `https://newsapi.org/v2/top-headlines?country=in&apiKey=23a9ff7dca9a4c879fc6ff9b22196288&page=${this.state.page}&pageSize=20`);
+          this.setState({
+            page: this.state.page + 1,
+            articles: parsedData.articles,
+          });
+        }
+      } else if (e.target.name === "previous") {
+        let response = await fetch(
+          `https://newsapi.org/v2/top-headlines?country=in&apiKey=23a9ff7dca9a4c879fc6ff9b22196288&page=${
+            this.state.page - 1
+          }&pageSize=20`
+        );
+        let parsedData = await response.json();
 
-      let parsedData = await response.json();
-    
-      this.setState({articles:parsedData.articles});
-    }
-    else if(e.target.name === "previous"){
-
-      this.setState({
-        page : this.state.page - 1
-      })
-
-      let response = await fetch( `https://newsapi.org/v2/top-headlines?country=in&apiKey=23a9ff7dca9a4c879fc6ff9b22196288&page=${this.state.page}&pageSize=20`);
-
-      let parsedData = await response.json();
-    
-      this.setState({articles:parsedData.articles});
-    }
-
-  }
+        this.setState({
+          page: this.state.page - 1,
+          articles: parsedData.articles,
+        });
+      }
+    };
 
   render() {
     return (
@@ -58,21 +63,42 @@ export class News extends Component {
         <h1 className="">LATEST NEWS</h1>
 
         <div className="row">
-          {this.state.articles.map((ele)=>{
-           return <div className="col-md-3" key={ele.url}>
-            <NewsItem title={ele.title} desc={ele.description} imgUrl={ele.urlToImage} newsUrl={ele.url}/>
-          </div>
+          {this.state.articles.map((ele) => {
+            return (
+              <div className="col-md-3" key={ele.url}>
+                <NewsItem
+                  title={ele.title}
+                  desc={ele.description}
+                  imgUrl={ele.urlToImage}
+                  newsUrl={ele.url}
+                />
+              </div>
+            );
           })}
         </div>
-          <hr/>
+        <hr />
         <div className="container d-flex justify-content-evenly mx-3">
-          <button  name="previous" type="button" className="btn btn-dark" onClick={this.handleNavigation}>&larr; Previous</button>
-          <button  name="next" type="button" className="btn btn-dark" onClick={this.handleNavigation}>Next &rarr;</button>
+          <button
+            disabled={this.state.page <= 1}
+            name="previous"
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNavigation}
+          >
+            &larr; Previous
+          </button>
+          <button
+            disabled={this.state.page <= 1?false:true}
+            name="next"
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNavigation}
+          >
+            Next &rarr;
+          </button>
         </div>
-
-
       </div>
-    )
+    );
   }
 }
 
